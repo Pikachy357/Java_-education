9abc3975082cb50dc092f11f2178d75fb74d9120
Shiphter <dyatlov-daniil@mail.ru>
2026-03-06 12:22:06 +0300

    next task IsValid

--- project 1/src/IsPalindrom.java
+++ project 1/src/IsPalindrom.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class IsPalindrom{
+class IsPalindrom {
     public void main(String[] args) {
         String s = "0P";
         System.out.println(isPalindrome(s));
@@ -9,28 +9,29 @@ class IsPalindrom{
     public boolean isPalindrome(String s) {
         char[] test = new char[s.length()];
         int count = 0;
-        for (int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c>= '0' && c<= '9'){
+            if (c >= '0' && c <= '9') {
                 test[count++] = c;
                 continue;
             }
-            for (int j = 0; j < 26; j++){
-                if (c == 'a'+ j){
-                    test[count++] = (char)('a'+ j);
+            for (int j = 0; j < 26; j++) {
+                if (c == 'a' + j) {
+                    test[count++] = (char) ('a' + j);
                     break;
                 }
-                if (c == 'A'+ j){;
-                    test[count++] = (char)('a'+ j);
+                if (c == 'A' + j) {
+                    ;
+                    test[count++] = (char) ('a' + j);
                     break;
                 }
             }
         }
-        for (int i = 0; i < count / 2; i++){
-            if (test[i] != test[count-1 - i]){
+        for (int i = 0; i < count / 2; i++) {
+            if (test[i] != test[count - 1 - i]) {
                 return false;
             }
         }
-        return true; 
+        return true;
     }
 }
