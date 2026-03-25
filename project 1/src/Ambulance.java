5fe31f7ec1a1ba3ded78f00b8e730390004ff81d
Shiphter <dyatlov-daniil@mail.ru>
2026-03-25 15:55:56 +0300

    work Ambulance

--- project 1/src/Ambulance.java
+++ project 1/src/Ambulance.java
@@ -8,41 +8,63 @@ public class Ambulance {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        String [] buff = reader.readLine().split(" ");
-        int k1 = Integer.parseInt(buff[0]);
-        int m = Integer.parseInt(buff[1]);
-        int k2 = Integer.parseInt(buff[2]);
-        int p2 = Integer.parseInt(buff[3]);
-        int n2 = Integer.parseInt(buff[4]);
-        if (n2 > m){ // если жтаж больще чем максимальное кол-во этажей
-            writer.write( "-1" + " -1");
-        } else{
-            int l = (p2-1)*m + (n2-1); // этажей перед квартирой к2
-            int count = 0;
-            int length = (int)Math.floor(k2/(l-1)) - (int)Math.ceil(k2/(l+1));
-            if (length <1){
-                writer.write( "-1" + " -1");
-            } else{
-                int [] P1 = new int[length];
-                int [] N1 = new int[length];
-                for (int x = (int)Math.ceil(k2/(l+1)), i = 0; x<k2/l; x++, i++){
-                    P1[i] = (k1-1)/(x * m) + 1; 
-                    N1[i] = ((k1-1) % (x*m))/x +1;
-                    
-                }
-                int p1 = P1[0],n1 = N1[0];
-                for (int i = 0; i < length-1; i++){
-                    if (P1[i] != P1[i+1]){
-                        p1 = 0;
+        String[] buff = reader.readLine().split(" ");
+        long k1 = Integer.parseInt(buff[0]);
+        long m = Integer.parseInt(buff[1]);
+        long k2 = Integer.parseInt(buff[2]);
+        long p2 = Integer.parseInt(buff[3]);
+        long n2 = Integer.parseInt(buff[4]);
+        if (n2 > m) { // если жтаж больще чем максимальное кол-во этажей
+            writer.write("-1" + " -1");
+        } else {
+            long l = (long) (p2 - 1) * m + (n2 - 1); // этажей перед квартирой к2
+                if (l > 0) {
+                    int length = (int) ((k2 - 1) / (double) l - k2 / ((double) l + 1) + 1);
+                    if (length < 1) {
+                        writer.write("-1" + " -1");
+                    } else {
+                        int from = (int) Math.ceil(k2 / ((double) l + 1));
+                        int to = (int)Math.floor((k2 -1)/ (double) l);
+                        long p = 0, n = 0, p1 = -1, n1 = -1;
+                        for (int x = from; x <= to; x++) {
+                            if (p1!= 0){
+                                p1 = (k1 - 1) / (x * m) + 1;
+                            }   
+                            if (n1 != 0){
+                                n1 = ((k1 - 1) % (x * m)) / x + 1;
+                            }
+                            if(x!=from && p1 != p){
+                                p1 = 0;
+                            }
+                            if (x!=from && n1 != n){
+                                n1 = 0;
+                            }
+                            p = p1;
+                            n = n1;
+                        }
+                        writer.write(p1 + " " + n1 + "\n");
                     }
-                    if (N1[i] != N1[i+1]){
-                        n1 = 0;
+                } else {
+                    long p = 0, n = 0, p1 = -1, n1 = -1;
+                    for (int x = (int) k2; x < k1; x++) {
+                            if (p1!= 0){
+                                p1 = (k1 - 1) / (x * m) + 1;
+                            }   
+                            if (n1 != 0){
+                                n1 = ((k1 - 1) % (x * m)) / x + 1;
+                            }
+                            if(x!=k2 && p1 != p){
+                                p1 = 0;
+                            }
+                            if (x!=k2 && n1 != n){
+                                n1 = 0;
+                            }
+                            p = p1;
+                            n = n1;
                     }
+                    writer.write(p1 + " " + n1 + "\n");
                 }
-                writer.write( p1 + " " + n1 + "\n");
             }
+            reader.close();writer.close();
         }
-        reader.close();
-        writer.close();
-    }
 }
