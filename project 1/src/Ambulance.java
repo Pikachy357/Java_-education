b1e711dabd6e580723a8566040a6616bf9edf326
Shiphter <dyatlov-daniil@mail.ru>
2026-03-31 20:25:58 +0300

    add

--- project 1/src/Ambulance.java
+++ project 1/src/Ambulance.java
@@ -4,67 +4,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Ambulance {
+public class Axaxa {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] buff = reader.readLine().split(" ");
-        long k1 = Integer.parseInt(buff[0]);
-        long m = Integer.parseInt(buff[1]);
-        long k2 = Integer.parseInt(buff[2]);
-        long p2 = Integer.parseInt(buff[3]);
-        long n2 = Integer.parseInt(buff[4]);
-        if (n2 > m) { // если жтаж больще чем максимальное кол-во этажей
-            writer.write("-1" + " -1");
-        } else {
-            long l = (long) (p2 - 1) * m + (n2 - 1); // этажей перед квартирой к2
-                if (l > 0) {
-                    int length = (int) ((k2 - 1) / (double) l - k2 / ((double) l + 1) + 1);
-                    if (length < 1) {
-                        writer.write("-1" + " -1");
-                    } else {
-                        int from = (int) Math.ceil(k2 / ((double) l + 1));
-                        int to = (int)Math.floor((k2 -1)/ (double) l);
-                        long p = 0, n = 0, p1 = -1, n1 = -1;
-                        for (int x = from; x <= to; x++) {
-                            if (p1!= 0){
-                                p1 = (k1 - 1) / (x * m) + 1;
-                            }   
-                            if (n1 != 0){
-                                n1 = ((k1 - 1) % (x * m)) / x + 1;
-                            }
-                            if(x!=from && p1 != p){
-                                p1 = 0;
-                            }
-                            if (x!=from && n1 != n){
-                                n1 = 0;
-                            }
-                            p = p1;
-                            n = n1;
-                        }
-                        writer.write(p1 + " " + n1 + "\n");
-                    }
-                } else {
-                    long p = 0, n = 0, p1 = -1, n1 = -1;
-                    for (int x = (int) k2; x < k1; x++) {
-                            if (p1!= 0){
-                                p1 = (k1 - 1) / (x * m) + 1;
-                            }   
-                            if (n1 != 0){
-                                n1 = ((k1 - 1) % (x * m)) / x + 1;
-                            }
-                            if(x!=k2 && p1 != p){
-                                p1 = 0;
-                            }
-                            if (x!=k2 && n1 != n){
-                                n1 = 0;
-                            }
-                            p = p1;
-                            n = n1;
-                    }
-                    writer.write(p1 + " " + n1 + "\n");
+        int n = Integer.parseInt(reader.readLine());
+        String otvet = reader.readLine();
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < n-1;  i++){
+            if (otvet.charAt(i) != 'h' && otvet.charAt(i) != 'a'){
+               count = 0;
+            } else{
+                if (count == 0) count = 1;
+                if ((otvet.charAt(i+1) == 'h' || otvet.charAt(i+1) == 'a') && (otvet.charAt(i+1) != otvet.charAt(i))){
+                    count ++;
+                } else{
+                    count = 1;
                 }
             }
-            reader.close();writer.close();
+
+            if (count > max){
+                max = count;
+            }
         }
+        if (max == 0 && (otvet.charAt(n-1) == 'h' || otvet.charAt(n-1) == 'a')){
+            max = 1;
+        }
+        writer.write(max + "\n");
+        reader.close();
+        writer.close();
+    }
 }
---
+++ project 1/src/kek.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class kek {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(reader.readLine());
+        String [] names = new String[n];
+        int [] bals = new int[n];
+        for (int i=0; i < n; i++){
+            names[i] = reader.readLine();
+        }
+        int m = Integer.parseInt(reader.readLine());
+        int prevA =0, prevB = 0;
+        for (int i = 0; i < m; i++){
+            String[] buff = reader.readLine().split(" ");
+            String[] count = buff[0].split(":");
+
+            int a = Integer.parseInt(count[0]);
+            int b = Integer.parseInt(count[1]);
+            int ball = a - prevA + b - prevB;
+            for (int j = 0; j < n; j++){
+                if (names[j].equals(buff[1])){
+                    bals[j] += ball;
+                    break;
+                }
+             }
+            prevA = a;
+            prevB = b;
+        }
+        int max = 0, k = 0;
+            for (int j = 0; j < n; j++){
+                if (bals[j] > max){
+                    k = j;
+                    max = bals[j];
+                }
+             }
+        writer.write(names[k] + " " + bals[k]);
+        reader.close();
+        writer.close();
+    }
+}
