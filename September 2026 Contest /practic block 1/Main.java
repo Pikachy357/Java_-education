d97e2f55340207a9885d3e6988c31df7255170ba
Shiphter <dyatlov-daniil@mail.ru>
2026-09-09 22:28:16 +0300

    task 1 practice block 1 done

--- September 2026 Contest /practic block 1/Main.java
+++ September 2026 Contest /practic block 1/Main.java
@@ -3,55 +3,59 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-public class Stack{
-
-    public Stack(String s){
-        this.s = s;
-    }
-    public char revert (char c){
-        switch (c){
-            case '(' -> return ')';
-            case "[" -> return ']';
-            default -> return c;
-        }
-    }
-    public boolean isValid(){
-        Deque <Charecter> stack= new Deque<>();
-        for (int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if ((c == '(') || (c == '[')){
-                stack.push(c);
-            } else{
-                if (stack.peek() == null) {
-                    return false;
-                } else{
-                    if (revet(stack.peek()) == c){
-                        stack.pop();
-                    }
-                }
-            }
-
-        }
-        if (stack.peek() != null) {
-            return false;
-        }
-        return true;
-    }
-    public boolean (){
-        Deque <Charecter> = new <>();
-    }
-}
 
 public class Main {
+
+    static class Pack {
+        String s;
+        public Pack(String s){
+            this.s = s;
+        }
+        public char revert (char c){
+            switch (c){
+                case '(' -> {return ')';}
+                case '[' -> {return ']';}
+                case '{' -> {return '}';}
+                default -> {return c;}
+            }
+        }
+        public boolean isValid(){
+            Deque <Character> stack= new ArrayDeque<>();
+            for (int i = 0; i < s.length(); i++){
+                char c = s.charAt(i);
+                if ((c == '(') || (c == '[') || (c == '{')){
+                    stack.push(c);
+                } else{
+                    if (stack.peek() == null) {
+                        return false;
+                    } else{
+                        if (revert(stack.peek()) == c){
+                            stack.pop();
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+
+            }
+            if (stack.peek() != null) {
+                return false;
+            }
+            return true;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        Stack buff = new Stack(reader.readline);
-        if (buff.isvalid) {
-            writer("yes");
+        Pack buff = new Pack(reader.readLine());
+        if (buff.isValid()) {
+            writer.write("yes");
         } else{
-            writer("no");
+            writer.write("no");
         }
         reader.close();
         writer.close();
